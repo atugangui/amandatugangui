48eb9540d2be0a3f9752bc105a57692b63aa616c
Amanda Tugangui <amandatug67@gmail.com>
2022-07-16 19:40:13 -0400

    Change navbar style, add resume and contact buttons

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Navbar</a>
-
+    <nav class="navbar navbar-expand-lg navbar-light">
       <div id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" v-on:click="view = 'home'">Home <span class="sr-only">(current)</span></a>
+          <li class="nav-item" v-bind:class="{ active: view == 'home' }">
+            <a class="nav-link" v-on:click="view = 'home'">Home</a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item" v-bind:class="{ active: view == 'about' }">
             <a class="nav-link" v-on:click="view = 'about'">About</a>
+          </li>
+          <li class="nav-item" v-bind:class="{ active: view == 'resume' }">
+            <a class="nav-link" v-on:click="view = 'resume'">Resume</a>
+          </li>
+          <li class="nav-item" v-bind:class="{ active: view == 'contact' }">
+            <a class="nav-link" v-on:click="view = 'contact'">Contact</a>
           </li>
         </ul>
       </div>
@@ -17,18 +21,24 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld v-if="view === 'home'" msg="Welcome to Your Vue.js App" />
     <About v-if="view === 'about'" msg="About"></About>
+    <Resume v-if="view === 'resume'" msg="Resume"></Resume>
+    <Contact v-if="view === 'contact'" msg="Contact"></Contact>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import About from './components/About.vue'
+import Resume from './components/Resume.vue'
+import Contact from './components/Contact.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    About
+    About,
+    Resume,
+    Contact
   },
   data() {
     return {
@@ -48,6 +58,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#navbarSupportedContent {
+  margin-left: auto;
 }
 </style>
