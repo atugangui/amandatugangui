0ea40e4c1810e0d7aa74d899db162b7b60f29525
Amanda Tugangui <amandatug67@gmail.com>
2022-07-16 18:46:07 -0400

    Add base component functionality

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld v-if="view==='home'" msg="Welcome to Your Vue.js App"/>
+    <About v-if="view === 'about'" msg="About"></About>
+    <a v-on:click="view='about'">About</a>
+    <a v-on:click="view='home'">Home</a>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import About from './components/About.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    About
+  },
+  data() {
+    return {
+      view: 'home'
+    }
+  },
 }
 </script>
 
