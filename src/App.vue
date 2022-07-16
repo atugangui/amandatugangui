22c9e53a691a8809690d7c86374ff57792d64967
Amanda Tugangui <amandatug67@gmail.com>
2022-07-16 19:02:26 -0400

    Add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
   <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+
+      <div id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" v-on:click="view = 'home'">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" v-on:click="view = 'about'">About</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld v-if="view==='home'" msg="Welcome to Your Vue.js App"/>
+    <HelloWorld v-if="view === 'home'" msg="Welcome to Your Vue.js App" />
     <About v-if="view === 'about'" msg="About"></About>
-    <a v-on:click="view='about'">About</a>
-    <a v-on:click="view='home'">Home</a>
   </div>
 </template>
 
@@ -28,6 +40,8 @@ export default {
 
 <style lang="scss">
 @import "~@/assets/scss/vendors/bootstrap-vue/index";
+@import "~@/assets/scss/vendors/bootstrap-vue/index";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import './plugins/bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 import App from './App.vue'
 
 Vue.config.productionTip = false
