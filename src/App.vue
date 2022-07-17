ed0bf3f020fd63861865f4a298f7bb5c0c82b408
Amanda Tugangui <amandatug67@gmail.com>
2022-07-17 00:44:59 -0400

    Created two divs lol

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,53 @@
 <template>
   <div id="app">
+    <!-- Top navbar -->
     <nav class="navbar navbar-expand-lg navbar-light">
       <div id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item" v-bind:class="{ active: view == 'home' }">
-            <a class="nav-link" v-on:click="view = 'home'">Home</a>
+            <a class="nav-link" v-on:click="view = 'home'">home</a>
+          </li>
+          <li class="nav-item" v-bind:class="{ active: view == 'gallery' }">
+            <a class="nav-link" v-on:click="view = 'gallery'">gallery</a>
           </li>
           <li class="nav-item" v-bind:class="{ active: view == 'about' }">
-            <a class="nav-link" v-on:click="view = 'about'">About</a>
+            <a class="nav-link" v-on:click="view = 'about'">about</a>
           </li>
           <li class="nav-item" v-bind:class="{ active: view == 'resume' }">
-            <a class="nav-link" v-on:click="view = 'resume'">Resume</a>
+            <a class="nav-link" v-on:click="view = 'resume'">resume</a>
           </li>
           <li class="nav-item" v-bind:class="{ active: view == 'contact' }">
-            <a class="nav-link" v-on:click="view = 'contact'">Contact</a>
+            <a class="nav-link" v-on:click="view = 'contact'">contact</a>
           </li>
         </ul>
       </div>
     </nav>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld v-if="view === 'home'" msg="Welcome to Your Vue.js App" />
+    <!-- /Top navbar-->
+
+    <!--<img alt="Vue logo" src="./assets/logo.png">-->
+    <Home v-if="view === 'home'" msg="Welcome to Your Vue.js App" />
     <About v-if="view === 'about'" msg="About"></About>
     <Resume v-if="view === 'resume'" msg="Resume"></Resume>
     <Contact v-if="view === 'contact'" msg="Contact"></Contact>
+    <Gallery v-if="view === 'gallery'" msg="Gallery"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 import About from './components/About.vue'
 import Resume from './components/Resume.vue'
 import Contact from './components/Contact.vue'
+import Gallery from './components/Gallery.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Home,
     About,
     Resume,
-    Contact
+    Contact,
+    Gallery
   },
   data() {
     return {
@@ -51,6 +60,12 @@ export default {
 <style lang="scss">
 @import "~@/assets/scss/vendors/bootstrap-vue/index";
 @import "~@/assets/scss/vendors/bootstrap-vue/index";
+@import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
+
+html, body {
+  min-height: 100%;
+  height: 100%;
+}
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -58,9 +73,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
+}
+
+.navbar {
+  height: 10vh;
 }
 
 #navbarSupportedContent {
   margin-left: auto;
+}
+
+.specialFont {
+  font-family: 'Jost', sans-serif;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+    <div class="row">
+    <div class="col" id="left"></div>
+    <div class="col" id="right">
+      <div class="quads" id="one"></div>
+      <div class="quads" id="two"></div>
+      <div class="quads" id="three"></div>
+      <div class="quads" id="four"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.container {
+  height: 90%;
+}
+
+.row {
+  height: 100%;
+}
+
+.hello {
+  height: 89vh;
+  margin-right: 8%;
+  margin-left: 8%;
+}
+
+.halves {
+  display: inline-block;
+  height: 100%;
+  width: 50%;
+}
+
+.quads {
+  display: inline-block;
+  height: 10%;
+  width: 50%;
+}
+
+#left {
+  background-color: red;
+}
+
+#right {
+  background-color: blue;
+}
+</style>
