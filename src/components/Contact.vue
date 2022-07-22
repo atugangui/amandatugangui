13b676efe9e695739fa83341e6e4b524c116f0f3
Amanda Tugangui <amandatug67@gmail.com>
2022-07-21 23:32:59 -0400

    Prototype contact page

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,14 +1,64 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
+    <div class="container">
+        <form>
+          <label>Name</label>
+          <input 
+            type="text" 
+            v-model="name"
+            name="name"
+            placeholder="Your Name"
+          >
+          <label>Email</label>
+          <input 
+            type="email" 
+            v-model="email"
+            name="email"
+            placeholder="Your Email"
+            >
+          <label>Message</label>
+          <textarea 
+            name="message"
+            v-model="message"
+            cols="30" rows="5"
+            placeholder="Message">
+          </textarea>
+          
+          <input type="submit" value="Send">
+        </form>
+    </div>
 </template>
 
 <script>
+import emailjs from 'emailjs-com';
+
 export default {
-  name: 'Contact',
-  props: {
-    msg: String
+  name: 'ContactUs',
+  data() {
+    return {
+      name: '',
+      email: '',
+      message: ''
+    }
+  },
+  methods: {
+    sendEmail(e) {
+      try {
+        // TO DO: ADD VARIABLES AS GITHUB KEYS
+        emailjs.sendForm('service_u6ie8iz', 'template_9jskeh6', e.target,
+        '25pK8EaCBXSYEZdoh', {
+          name: this.name,
+          email: this.email,
+          message: this.message
+        })
+
+      } catch(error) {
+          console.log({error})
+      }
+      // Reset form field
+      this.name = ''
+      this.email = ''
+      this.message = ''
+    },
   }
 }
 </script>
