3a14570cd51e451cdccc25129790515d583cdaa3
Amanda Tugangui <amandatug67@gmail.com>
2022-07-28 23:13:30 -0400

    Adaptive design for About page for mobile

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,39 +1,72 @@
-<template>
+<template v-if="isMobile">
   <div class="container">
     <div class="row">
-      <div class="col">
-        <v-lazy-image
-          class="nonGalleryImage"
-          :src=largePortrait
-          :src-placeholder=tinyPortrait
-          alt="Candid Amanda Tugangui"
-        />
-      </div>
-      <div class="col">
-        <p id="bio">
-          Having recently relocated from Los Angeles, Amanda was just 
-          nominated for an acting award from Theatre Palisades for her role as 
-          Gloria in <span class="italic">Wait Until Dark.</span>
-          <br />
-          <br />
-          When she's not on stage, Amanda is intensely curious about everything. 
-          <br />
-          Like, what is it like to <a href="https://www.youtube.com/watch?v=jGH_rhOWm0s" target="_blank">play harp?</a> (Hard.)
-          <br />
-          Or to write a <a href="https://tugangui.substack.com/" target="_blank">travel blog?</a> (Cathartic.)
-          <br />
-          To <a href="https://imgur.com/a/esCVBuY" target="_blank">grow bok choy?</a> (Delicious.)
-          <br />
-          <br />
-          With a background as a programmer (she coded this website herself!),
-          she most recently worked at a neuroscience lab at UCLA, where she 
-          co-authored several papers published in <span class="italic">Nature.</span>
-          <br />
-          <br />
-          Still, having tried out so many things, performing remains Amanda's longest
-          and brightest love.
-        </p>
-      </div>
+      <template v-if="isMobile">
+      <div class="col-12">
+          <p id="bio">
+            Having recently relocated from Los Angeles, Amanda was just
+            nominated for an acting award from Theatre Palisades for her role as
+            Gloria in <span class="italic">Wait Until Dark.</span>
+            <br />
+            <br />
+            When she's not on stage, Amanda is intensely curious about everything.
+            <br />
+            Like, what is it like to <a href="https://www.youtube.com/watch?v=jGH_rhOWm0s" target="_blank">play
+              harp?</a>
+            (Hard.)
+            <br />
+            Or to write a <a href="https://tugangui.substack.com/" target="_blank">travel blog?</a> (Cathartic.)
+            <br />
+            To <a href="https://imgur.com/a/esCVBuY" target="_blank">grow bok choy?</a> (Delicious.)
+            <br />
+            <br />
+            With a background as a programmer (she coded this website herself!),
+            she most recently worked at a neuroscience lab at UCLA, where she
+            co-authored several papers published in <span class="italic">Nature.</span>
+            <br />
+            <br />
+            Still, having tried out so many things, performing remains Amanda's longest
+            and brightest love.
+          </p>
+        </div>
+        <div class="col-12">
+          <v-lazy-image class="nonGalleryImage" :src=largePortrait :src-placeholder=tinyPortrait
+            alt="Candid Amanda Tugangui" />
+        </div>
+      </template>
+      <template v-else>
+        <div class="col">
+          <v-lazy-image class="nonGalleryImage" :src=largePortrait :src-placeholder=tinyPortrait
+            alt="Candid Amanda Tugangui" />
+        </div>
+        <div class="col">
+          <p id="bio">
+            Having recently relocated from Los Angeles, Amanda was just
+            nominated for an acting award from Theatre Palisades for her role as
+            Gloria in <span class="italic">Wait Until Dark.</span>
+            <br />
+            <br />
+            When she's not on stage, Amanda is intensely curious about everything.
+            <br />
+            Like, what is it like to <a href="https://www.youtube.com/watch?v=jGH_rhOWm0s" target="_blank">play
+              harp?</a>
+            (Hard.)
+            <br />
+            Or to write a <a href="https://tugangui.substack.com/" target="_blank">travel blog?</a> (Cathartic.)
+            <br />
+            To <a href="https://imgur.com/a/esCVBuY" target="_blank">grow bok choy?</a> (Delicious.)
+            <br />
+            <br />
+            With a background as a programmer (she coded this website herself!),
+            she most recently worked at a neuroscience lab at UCLA, where she
+            co-authored several papers published in <span class="italic">Nature.</span>
+            <br />
+            <br />
+            Still, having tried out so many things, performing remains Amanda's longest
+            and brightest love.
+          </p>
+        </div>
+      </template>
     </div>
   </div>
 </template>
@@ -49,12 +82,26 @@ export default {
   components: {
     VLazyImage
   },
+  data() {
+    return {
+      isMobile: false
+    }
+  },
   computed: {
     largePortrait() {
       return largePortrait;
     },
     tinyPortrait() {
       return tinyPortrait;
+    }
+  },
+  mounted() {
+    this.onResize()
+    window.addEventListener('resize', this.onResize, { passive: true })
+  },
+  methods: {
+    onResize() {
+      this.isMobile = window.innerWidth < 900
     }
   }
 }
@@ -88,6 +135,13 @@ export default {
 @media (min-width: 1366px) {
   #bio {
     width: 100%;
+  }
+}
+
+@media (max-width: 900px) {
+  #bio {
+    width: 100%;
+    text-align: center;
   }
 }
 </style>
