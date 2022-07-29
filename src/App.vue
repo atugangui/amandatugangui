ad90523f38f3410cc6b9ee5a5d1c1232c0b5f89a
Amanda Tugangui <amandatug67@gmail.com>
2022-07-29 00:28:38 -0400

    Add adaptive design for navbar

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,49 @@
 <template>
   <div id="app">
     <!-- Top navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light">
-      <div id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item" v-bind:class="{ active: view == 'home' }">
-            <a class="nav-link" v-on:click="view = 'home'">home</a>
-          </li>
-          <li class="nav-item" v-bind:class="{ active: view == 'gallery' }">
-            <a class="nav-link" v-on:click="view = 'gallery'">gallery</a>
-          </li>
-          <li class="nav-item" v-bind:class="{ active: view == 'about' }">
-            <a class="nav-link" v-on:click="view = 'about'">about</a>
-          </li>
-          <li class="nav-item" v-bind:class="{ active: view == 'resume' }">
-            <a class="nav-link" v-on:click="view = 'resume'">resume</a>
-          </li>
-          <li class="nav-item" v-bind:class="{ active: view == 'contact' }">
-            <a class="nav-link" v-on:click="view = 'contact'">contact</a>
-          </li>
-        </ul>
+    <template v-if="isMobile">
+      <!-- Top Navigation Menu -->
+      <div class="topnav">
+        <div id="buttonDiv">
+        <button v-on:click=" listIsHidden= !listIsHidden " class="btn customHamburger customBtnDesign">
+          <BIconList id="hamburgerIcon"/>
+          </button>
+          </div>
+        <!-- Navigation links (hidden by default) -->
+        <div id="myLinks" v-bind:class="{ showList: !listIsHidden}">
+          <a v-on:click="view = 'home'" v-bind:class="{ active: view=='home' }">home</a>
+          <a v-on:click="view = 'gallery'" v-bind:class="{ active: view=='gallery'}">gallery</a>
+          <a v-on:click="view = 'about'" v-bind:class="{ hactive: view=='about' }">about</a>
+          <a v-on:click="view = 'resume'" v-bind:class="{ active: view=='resume' }">resume</a>
+          <a v-on:click="view = 'contact'" v-bind:class="{ active: view=='contact' }">contact</a>
+        </div>
+        <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
+        
       </div>
-    </nav>
+    </template>
+    <template v-else>
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <div id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item" v-bind:class="{ active: view == 'home' }">
+              <a class="nav-link" v-on:click="view = 'home'">home</a>
+            </li>
+            <li class="nav-item" v-bind:class="{ active: view == 'gallery' }">
+              <a class="nav-link" v-on:click="view = 'gallery'">gallery</a>
+            </li>
+            <li class="nav-item" v-bind:class="{ active: view == 'about' }">
+              <a class="nav-link" v-on:click="view = 'about'">about</a>
+            </li>
+            <li class="nav-item" v-bind:class="{ active: view == 'resume' }">
+              <a class="nav-link" v-on:click="view = 'resume'">resume</a>
+            </li>
+            <li class="nav-item" v-bind:class="{ active: view == 'contact' }">
+              <a class="nav-link" v-on:click="view = 'contact'">contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </template>
     <!-- /Top navbar-->
 
     <!-- Main views -->
@@ -40,6 +62,7 @@ import About from './components/About.vue'
 import Resume from './components/Resume.vue'
 import Contact from './components/Contact.vue'
 import Gallery from './components/Gallery.vue'
+import { BIconList } from 'bootstrap-vue'
 
 export default {
   name: 'App',
@@ -49,10 +72,22 @@ export default {
     Resume,
     Contact,
     Gallery,
+    BIconList
   },
   data() {
     return {
-      view: 'home'
+      isMobile: false,
+      view: 'home',
+      listIsHidden: true
+    }
+  },
+  mounted() {
+    this.onResize()
+    window.addEventListener('resize', this.onResize, { passive: true })
+  },
+  methods: {
+    onResize() {
+      this.isMobile = window.innerWidth < 900
     }
   }
 }
@@ -121,9 +156,11 @@ a:hover {
   .container {
     max-width: 1828px;
   }
+
   .navbar {
     font-size: 1.5rem;
   }
+
   body {
     font-size: 1.5rem;
   }
@@ -134,8 +171,69 @@ a:hover {
   transform: translate3d(0, 50%, 0);
   transition: opacity 0.5s, transform 0.5s;
 }
+
 .v-lazy-image-loaded {
   opacity: 1;
   transform: translate3d(0, 0, 0);
+}
+
+@media (max-width: 900px) {
+   /* Style the navigation menu */
+.topnav {
+  overflow: hidden;
+  position: relative;
+}
+
+#myLinks > a{
+  color: rgba(0, 0, 0, 0.5);
+}
+
+/* Hide the links inside the navigation menu (except for logo/home) */
+.topnav #myLinks {
+  display: none;
+}
+
+/* Style navigation menu links */
+.topnav a {
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+  display: block;
+}
+
+/* Style the hamburger menu */
+.topnav a.icon {
+  display: block;
+  position: absolute;
+  right: 0;
+  top: 0;
+}
+
+/* Add a grey background color on mouse-over */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.active {
+  color: black !important;
+}
+
+.customHamburger {
+  float:right;
+  padding: 5px;
+}
+
+#hamburgerIcon {
+  color: #42b983;
+}
+
+.showList {
+  display: block !important;
+}
+
+#buttonDiv {
+  width: 100%;
+}
 }
 </style>
